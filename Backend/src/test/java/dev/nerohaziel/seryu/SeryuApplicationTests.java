6901aca5e3fb932793263e61d573f47647323e82
HepoHB <NeroHaziel@outlook.com>
2024-09-14 01:47:39 -0300

    Frontend and Backend Started

---
+++ Backend/src/test/java/dev/nerohaziel/seryu/SeryuApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nerohaziel.seryu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeryuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
